126358116647a48925a2f4a0209bb9a37361ff4c
Arwindpianist <arwinboy@gmail.com>
2025-09-17 14:21:43 +0800

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4FLE1RWKVX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4FLE1RWKVX');
+            `,
+          }}
+        />
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   )
